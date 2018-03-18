2b875bd99dd121bedf18d145813387239393fded
emma-wa <29693223+emma-wa@users.noreply.github.com>
2018-03-18 15:34:23 +0100

    Finalize basic HTML w/javascript navigation

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,11 +1,19 @@
 $(document).ready(function() {
   $('#portfolio').hide();
+  $('#contact').hide();
   $('#portfoliobutton').click(function() {
-    $('#portfolio').fadeIn('slow');
     $('#about').hide();
+    $('#contact').hide();
+    $('#portfolio').fadeIn('slow');
   });
   $('#aboutbutton').click(function() {
     $('#portfolio').hide();
+    $('#contact').hide();
     $('#about').fadeIn('slow');
+  });
+  $('#contactbutton').click(function() {
+    $('#portfolio').hide();
+    $('#about').hide();
+    $('#contact').fadeIn('slow');
   });
 });
